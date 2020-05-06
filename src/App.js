e1629ea9286dc0e932d914b1e6d2c0d8d319b31b
Mirosław Pajor <54025053+strangebedfellow@users.noreply.github.com>
2020-05-06 21:33:24 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='title'>
+        <h1 className='text-focus-in'>Mirosław <span>Pajor</span></h1>
+        <h2 className='tracking-in-expand'>JavaScript Developer</h2>
+      </div>
+      <div className='content'>
+        <span>
+          <div className='row scale-in-center'>
+            <i class="fas fa-mobile-alt"></i>
+            <p>504191282</p>
+          </div>
+        </span>
+        <span>
+          <a href='mailto:miroslaw@pajor.info.pl'>
+            <div className='row scale-in-center'>
+              <i class="fas fa-at"></i>
+              <p>miroslaw@pajor.info.pl</p>
+            </div>
+          </a>
+        </span>
+        <span>
+          <a href='https://www.linkedin.com/in/miroslaw-pajor/' target='_blank'>
+            <div className='row scale-in-center'>
+              <i class="fab fa-linkedin"></i>
+              <p>linkedin.com/in/miroslaw-pajor/</p>
+            </div>
+          </a>
+        </span>
+        <span>
+          <a href='https://github.com/strangebedfellow/' target='_blank'>
+            <div className='row scale-in-center'>
+              <i class="fab fa-github"></i>
+              <p>github.com/strangebedfellow/</p>
+            </div>
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
